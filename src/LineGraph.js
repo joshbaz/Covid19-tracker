3d7ec34bebf6ef6e1b88c3a4e5e51c032524a0ef
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2020-12-04 11:39:35 +0300

    objectRecreation

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -11,95 +11,297 @@ function LineGraph() {
           .then((data) => {
             console.log('data',data);
             let allGeneralDates = [];
+
             let ugandanDataCases= [];
             let kenyanDataCases = [];
             let tanzaniaDataCases = [];
             let rwandaDataCases = [];
             let burundiDataCases = [];
+            let TotalDataCases = [];
+            let objectCases = []
 
-            function spiltData(dataToSplit, country){
+            let ugandanDeathCases = []
+            let kenyanDeathCases = [];
+            let tanzaniaDeathCases = [];
+            let rwandaDeathCases = [];
+            let burundiDeathCases = [];
+            let TotalDeathCases = [];
+
+             let ugandanRecoveredCases = [];
+             let kenyanRecoveredCases = [];
+             let tanzaniaRecoveredCases = [];
+             let rwandaRecoveredCases = [];
+             let burundiRecoveredCases = [];
+             let TotalRecoveredCases = [];
+
+            function spiltData(dataToSplit, country, type){
               console.log('the data and country', dataToSplit, country);
-              
-              if(allGeneralDates.length === 0) {
+              //cases
+              if (type === 'cases') {
+                if (allGeneralDates.length === 0) {
                   let propertyDate = Object.keys(dataToSplit);
                   function getDataValues() {
-                    let dataValues = []
+                    let dataValues = [];
                     for (const date of propertyDate) {
-                      console.log("these are spilt dataes", date);
-                      console.log("these are the numbers", dataToSplit[date]);
                       dataValues.push(dataToSplit[date]);
                     }
                     return dataValues;
                   }
                   allGeneralDates.push(...propertyDate);
-                  if (country === 'Uganda'){
-                   let ugandaResults = getDataValues();
-                   ugandanDataCases.push(ugandaResults);
-                   console.log(" ugandanDataCases", ugandanDataCases);
-                  } else if (country === 'Kenya') {
+                  if (country === "Uganda") {
+                    let ugandaResults = getDataValues();
+                    ugandanDataCases.push(...ugandaResults);
+                  } else if (country === "Kenya") {
                     let kenyaResults = getDataValues();
-                    kenyanDataCases.push(kenyaResults);
-                  } else if (country === 'Tanzania') {
-                     let TanzaniaResults = getDataValues();
-                    tanzaniaDataCases.push(TanzaniaResults);
+                    kenyanDataCases.push(...kenyaResults);
+                  } else if (country === "Tanzania") {
+                    let TanzaniaResults = getDataValues();
+                    tanzaniaDataCases.push(...TanzaniaResults);
                   } else if (country === "Rwanda") {
                     let rwandaResults = getDataValues();
-                   rwandaDataCases.push(rwandaResults);
+                    rwandaDataCases.push(...rwandaResults);
                   } else if (country === "Burundi") {
                     let burundiResults = getDataValues();
-                     burundiDataCases.push(burundiResults);
+                    burundiDataCases.push(...burundiResults);
                   }
                 } else {
                   function getDataValues() {
-                   let  propertyDate = Object.keys(dataToSplit);
+                    let propertyDate = Object.keys(dataToSplit);
                     let dataValues = [];
                     for (const date of propertyDate) {
-                      console.log("these are spilt dataes", date);
-                      console.log("these are the numbers", dataToSplit[date]);
                       dataValues.push(dataToSplit[date]);
                     }
                     return dataValues;
                   }
-                  
+
                   if (country === "Uganda") {
                     let ugandaResults = getDataValues();
-                    ugandanDataCases.push(ugandaResults);
-                    console.log(" ugandanDataCases", ugandanDataCases);
+                    ugandanDataCases.push(...ugandaResults);
                   } else if (country === "Kenya") {
                     let kenyaResults = getDataValues();
-                    kenyanDataCases.push(kenyaResults);
-                    console.log(" ukenyanDataCases", kenyanDataCases);
+                    kenyanDataCases.push(...kenyaResults);
                   } else if (country === "Tanzania") {
                     let TanzaniaResults = getDataValues();
-                    tanzaniaDataCases.push(TanzaniaResults);
-                    console.log(" tanzaniaDataCases", tanzaniaDataCases);
+                    tanzaniaDataCases.push(...TanzaniaResults);
                   } else if (country === "Rwanda") {
                     let rwandaResults = getDataValues();
-                    rwandaDataCases.push(rwandaResults);
-                    console.log("  rwandaDataCases", rwandaDataCases);
+                    rwandaDataCases.push(...rwandaResults);
                   } else if (country === "Burundi") {
                     let burundiResults = getDataValues();
-                    burundiDataCases.push(burundiResults);
-                    console.log("  burundiDataCases", burundiDataCases);
+                    burundiDataCases.push(...burundiResults);
                   }
                 }
+              } 
+              //deaths
+              else if (type === 'deaths') {
+                if (allGeneralDates.length === 0) {
+                  let propertyDate = Object.keys(dataToSplit);
+                  function getDataValues() {
+                    let dataValues = [];
+                    for (const date of propertyDate) {
+                      dataValues.push(dataToSplit[date]);
+                    }
+                    return dataValues;
+                  }
+                  allGeneralDates.push(...propertyDate);
+                  if (country === "Uganda") {
+                    let ugandaResults = getDataValues();
+                    ugandanDeathCases.push(...ugandaResults);
+                  } else if (country === "Kenya") {
+                    let kenyaResults = getDataValues();
+                    kenyanDeathCases.push(...kenyaResults);
+                  } else if (country === "Tanzania") {
+                    let TanzaniaResults = getDataValues();
+                    tanzaniaDeathCases.push(...TanzaniaResults);
+                  } else if (country === "Rwanda") {
+                    let rwandaResults = getDataValues();
+                    rwandaDeathCases.push(...rwandaResults);
+                  } else if (country === "Burundi") {
+                    let burundiResults = getDataValues();
+                    burundiDeathCases.push(...burundiResults);
+                  }
+                } else {
+                  function getDataValues() {
+                    let propertyDate = Object.keys(dataToSplit);
+                    let dataValues = [];
+                    for (const date of propertyDate) {
+                      dataValues.push(dataToSplit[date]);
+                    }
+                    return dataValues;
+                  }
+
+                  if (country === "Uganda") {
+                    let ugandaResults = getDataValues();
+                    ugandanDeathCases.push(...ugandaResults);
+                  } else if (country === "Kenya") {
+                    let kenyaResults = getDataValues();
+                    kenyanDeathCases.push(...kenyaResults);
+                  } else if (country === "Tanzania") {
+                    let TanzaniaResults = getDataValues();
+                    tanzaniaDeathCases.push(...TanzaniaResults);
+                  } else if (country === "Rwanda") {
+                    let rwandaResults = getDataValues();
+                    rwandaDeathCases.push(...rwandaResults);
+                  } else if (country === "Burundi") {
+                    let burundiResults = getDataValues();
+                    burundiDeathCases.push(...burundiResults);
+                  }
+                }
+              }
+              //recovered
+              else if (type === 'recovered') {
+                if (allGeneralDates.length === 0) {
+                  let propertyDate = Object.keys(dataToSplit);
+                  function getDataValues() {
+                    let dataValues = [];
+                    for (const date of propertyDate) {
+                      dataValues.push(dataToSplit[date]);
+                    }
+                    return dataValues;
+                  }
+                  allGeneralDates.push(...propertyDate);
+                  if (country === "Uganda") {
+                    let ugandaResults = getDataValues();
+                    ugandanRecoveredCases.push(...ugandaResults);
+                  } else if (country === "Kenya") {
+                    let kenyaResults = getDataValues();
+                    kenyanRecoveredCases.push(...kenyaResults);
+                  } else if (country === "Tanzania") {
+                    let TanzaniaResults = getDataValues();
+                    tanzaniaRecoveredCases.push(...TanzaniaResults);
+                  } else if (country === "Rwanda") {
+                    let rwandaResults = getDataValues();
+                    rwandaRecoveredCases.push(...rwandaResults);
+                  } else if (country === "Burundi") {
+                    let burundiResults = getDataValues();
+                    burundiRecoveredCases.push(...burundiResults);
+                  }
+                } else {
+                  function getDataValues() {
+                    let propertyDate = Object.keys(dataToSplit);
+                    let dataValues = [];
+                    for (const date of propertyDate) {
+                      dataValues.push(dataToSplit[date]);
+                    }
+                    return dataValues;
+                  }
+
+                  if (country === "Uganda") {
+                    let ugandaResults = getDataValues();
+                    ugandanRecoveredCases.push(...ugandaResults);
+                  } else if (country === "Kenya") {
+                    let kenyaResults = getDataValues();
+                    kenyanRecoveredCases.push(...kenyaResults);
+                  } else if (country === "Tanzania") {
+                    let TanzaniaResults = getDataValues();
+                    tanzaniaRecoveredCases.push(...TanzaniaResults);
+                  } else if (country === "Rwanda") {
+                    let rwandaResults = getDataValues();
+                    rwandaRecoveredCases.push(...rwandaResults);
+                  } else if (country === "Burundi") {
+                    let burundiResults = getDataValues();
+                    burundiRecoveredCases.push(...burundiResults);
+                  }
+                }
+              }
               
             }
 
            function getData(arraydata) {
               const loopedCases = [];
-            
-
               for (const cases of arraydata) {
                 console.log(cases.timeline.cases);
                 console.log(cases.country);
-                spiltData(cases.timeline.cases, cases.country)
+                spiltData(cases.timeline.cases, cases.country, 'cases');
+                spiltData(cases.timeline.deaths, cases.country, "deaths");
+                spiltData(cases.timeline.recovered, cases.country, "recovered");
                 loopedCases.push(cases)
               }
               return loopedCases;
            }
+           console.log("getData()", getData(data));
+           console.log('this is the expected-data',ugandanDeathCases)
+           
+           //Totals recieved
+           function getTotals(array1, array2, array3, array4, array5, dates, type) {
+            let length = array1.length;
+           
+            console.log('thisis array1', array1, length)
+            if (type === 'cases') {
+              let alldat = dates;
+              for (let iteration = 0; iteration < length; iteration++) {
+                let iterations = iteration + 1;
+                let total =
+                  array1[iteration] +
+                  array2[iteration] +
+                  array3[iteration] +
+                  array4[iteration] +
+                  array5[iteration];
+                  
+                let objecttrial = {};
+                Object.defineProperty(objecttrial, `${alldat[iteration]}`, {
+                  value: total,
+                });
+                objectCases.push(objecttrial)
+                console.log("this is the outcome :", objecttrial);
+                  
+                  TotalDataCases.push( total);
 
-           console.log('getData()', getData(data));
+                  if (iterations === length) {
+  
+                    console.log(objectCases)
+                       console.log('the reduce and object.assign', objectCases.reduce((initialStart, nextObject)=> {
+                         return console.log('result', initialStart, 'current', nextObject)
+                       }, {}))
+                    
+                  }
+                
+              }
+            } else if (type === 'deaths') {
+              for (let iteration = 0; iteration < length; iteration++) {
+                let iterations = iteration + 1;
+                let total =
+                  array1[iteration] +
+                  array2[iteration] +
+                  array3[iteration] +
+                  array4[iteration] +
+                  array5[iteration];
+
+                TotalDeathCases.push(total);
+              }
+            } else if (type === 'recovered') {
+              for (let iteration = 0; iteration < length; iteration++) {
+                let iterations = iteration + 1;
+                let total =
+                  array1[iteration] +
+                  array2[iteration] +
+                  array3[iteration] +
+                  array4[iteration] +
+                  array5[iteration];
+
+                TotalRecoveredCases.push(total);
+              }
+            }
+           }
+           console.log('these are total cases', ugandanDataCases)
+           getTotals(
+             ugandanDataCases,
+             kenyanDataCases,
+             tanzaniaDataCases,
+             rwandaDataCases,
+             burundiDataCases,
+             allGeneralDates,
+             'cases'
+           );
+
+          //  function combineData() {
+          //    if (type === "cases") {
+          //    } else if (type === "deaths") {
+          //    } else if (type === "recovered") {
+          //    }
+          //  }
+
+
           });
     }, [])
     return (
